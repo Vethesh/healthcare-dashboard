bd23517f3a2af1513af7900efe77aa5af438405c
Vethesh <vetheshkumar7@gmail.com>
2025-05-26 22:03:17 +0530

    Test basic Vercel render

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
+// import React from "react";
+// import Sidebar from "./components/Sidebar";
+// import Header from "./components/Header";
+// import DashboardMainContent from "./components/DashboardMainContent";
+// import "./App.css";
+
+// export default function App() {
+//   return (
+//     <div className="flex w-full h-[100] bg-white text-[#2f3c4d] font-sans">
+//       <Sidebar />
+//       <div className="flex flex-col flex-grow">
+//         <Header />
+//         <DashboardMainContent />
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import DashboardMainContent from "./components/DashboardMainContent";
-import "./App.css";
 
 export default function App() {
   return (
-    <div className="flex w-full h-[100] bg-white text-[#2f3c4d] font-sans">
-      <Sidebar />
-      <div className="flex flex-col flex-grow">
-        <Header />
-        <DashboardMainContent />
-      </div>
+    <div style={{ padding: 50 }}>
+      <h1>Hello, Vercel!</h1>
     </div>
   );
 }
+
 // import React from "react";
 // import Sidebar from "./components/Sidebar";
 // import Header from "./components/Header";
